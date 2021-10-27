2229778f27b04e3e3c36154f256eb7ac6545770c
Mihai Bratu <89224049+Steppenwolff4@users.noreply.github.com>
2021-10-27 19:01:13 +0300

    Update Audio_recording.py

--- Audio_recording.py
+++ Audio_recording.py
@@ -2,13 +2,20 @@ import pyaudio
 import wave
 
 def recording_audio():
+    # the file name output you want to record into
     filename = "recorded.wav"
+    # set the chunk size of 1024 samples
     chunk = 1024
+    # sample format
     FORMAT = pyaudio.paInt16
+    # mono, change to 2 if you want stereo
     channels = 1
+    # 44100 samples per second
     sample_rate = 44100
-    record_seconds = 12
+    record_seconds = 5
+    # initialize PyAudio object
     p = pyaudio.PyAudio()
+    # open stream object as input & output
     stream = p.open(format=FORMAT,
                     channels=channels,
                     rate=sample_rate,
@@ -19,12 +26,21 @@ def recording_audio():
     for i in range(int(44100 / chunk * record_seconds)):
         data = stream.read(chunk)
         frames.append(data)
+    # stop and close stream
     stream.stop_stream()
     stream.close()
+    # terminate pyaudio object
     p.terminate()
+    # save audio file
+    # open the file in 'write bytes' mode
     wf = wave.open(filename, "wb")
+    # set the channels
     wf.setnchannels(channels)
+    # set the sample format
     wf.setsampwidth(p.get_sample_size(FORMAT))
+    # set the sample rate
     wf.setframerate(sample_rate)
+    # write the frames as bytes
     wf.writeframes(b"".join(frames))
+    # close the file
     wf.close()
