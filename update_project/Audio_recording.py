338e604af33974fcea5c9353e3879520f679badb
Mihai Bratu <89224049+Mihai-Bratu-97@users.noreply.github.com>
2021-10-30 19:45:01 +0300

    Add files via upload

---
+++ update_project/Audio_recording.py
@@ -0,0 +1,60 @@
+import pyaudio
+import wave
+
+def preparing_audio_recording1():
+    p = pyaudio.PyAudio()
+    return p
+
+def preparing_audio_recording2(FORMAT, channels, sample_rate, chunk):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=channels,
+                    rate=sample_rate,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+    return stream
+
+def starting_audio(stream, chunk, record_seconds, sample_rate):
+    frames = []
+    print("Starting audio recording")
+    for i in range(int(sample_rate / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+        print("Recording audio...")
+    return frames
+
+def end_audio(stream, filename, channels, frames, sample_rate, FORMAT, p):
+    # stop and close stream
+    stream.stop_stream()
+    stream.close()
+        # terminate pyaudio object
+    p.terminate()
+        # save audio file
+        # open the file in 'write bytes' mode
+    wf = wave.open(filename, "wb")
+        # set the channels
+    wf.setnchannels(channels)
+        # set the sample format
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+        # set the sample rate
+    wf.setframerate(sample_rate)
+        # write the frames as bytes
+    wf.writeframes(b"".join(frames))
+        # close the file
+    wf.close()
+
+def audio_recording_script():
+    filename = "recorded.wav"
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    channels = 2
+    sample_rate = 44100
+    record_seconds = 10
+    try:
+        preparing1 = preparing_audio_recording1()
+        preparing2 = preparing_audio_recording2(FORMAT, channels, sample_rate, chunk)
+        audio = starting_audio(preparing2, chunk, record_seconds, sample_rate)
+        end_audio(preparing2, filename, channels, audio, sample_rate, FORMAT, preparing1)
+    except:
+        print("There is no input or output")
