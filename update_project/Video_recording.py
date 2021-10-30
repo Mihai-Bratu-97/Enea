bedccfe1cfb30495e02d568bc93457ad3218abf4
Mihai Bratu <89224049+Mihai-Bratu-97@users.noreply.github.com>
2021-10-30 18:45:03 +0300

    Add files via upload

---
+++ update_project/Video_recording.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import pyautogui
+
+def screen(length, height):
+    screen_resolution = (length, height)
+    return screen_resolution
+
+def fourcc():
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    return fourcc
+
+def output(fps, screen_size ):
+    out = cv2.VideoWriter("output.avi", fourcc(), fps, (screen_size))
+    return out
+
+def start_recording(seconds, fps, outting):
+    final_seconds = seconds * fps
+    count_seconds = 0
+    print("The recording has started")
+    while True:
+        # make a screenshot
+        img = pyautogui.screenshot()
+        # convert these pixels to a proper numpy array to work with OpenCV
+        frame = np.array(img)
+        # convert colors from BGR to RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # write the frame
+        outting.write(frame)
+        # if the user clicks q, it exits
+        if count_seconds % fps == 0:
+            print("Recording...")
+        count_seconds += 1
+        if count_seconds == final_seconds:
+            print("The recording has finished")
+            break
+
+def end_recording(out):
+    # make sure everything is closed when exited
+    cv2.destroyAllWindows()
+    out.release()
+
+def start_video_recording():
+    frame_per_second = 15
+    minutes_in_seconds = 10
+    resolution = screen(1920, 1080)
+    fourcc()
+    out_put = output(frame_per_second, resolution)
+    start_recording(minutes_in_seconds, frame_per_second, out_put)
+    end_recording(out_put)
