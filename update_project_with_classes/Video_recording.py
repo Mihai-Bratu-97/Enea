cf02050ab7357f5b066722d245281dc08a137a48
Mihai Bratu <89224049+Mihai-Bratu-97@users.noreply.github.com>
2021-11-01 00:15:08 +0200

    Update Video_recording.py

--- update_project_with_classes/Video_recording.py
+++ update_project_with_classes/Video_recording.py
@@ -4,19 +4,20 @@ import pyautogui
 
 class VideoScript:
     def __init__(self):
-        self.SCREEN_SIZE = (1920, 1080)
-        self.fourcc = cv2.VideoWriter_fourcc(*"XVID")
-        self.out = cv2.VideoWriter("output.avi", self.fourcc, 17, self.SCREEN_SIZE)
+        self.SCREEN_SIZE = (1920, 1080) # screen_resolution
+        self.fourcc = cv2.VideoWriter_fourcc(*"XVID") # the codec
+        self.out = cv2.VideoWriter("output.avi", self.fourcc, 17, self.SCREEN_SIZE) # create the video write object
         self.starting_video_recording()
 
+    #this function starts to recording
     def starting_video_recording(self):
         print("Start video recording")
         count_seconds = 0
         while True:
-            img = pyautogui.screenshot()
-            frame = np.array(img)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self.out.write(frame)
+            img = pyautogui.screenshot() # make a screenshot
+            frame = np.array(img) # convert these pixels to a proper numpy array to work with OpenCV
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert colors from BGR to RGB
+            self.out.write(frame) # write the frame
             count_seconds += 1
             if count_seconds % 17 == 0:
                 print("Video recording...")
@@ -25,6 +26,7 @@ class VideoScript:
                 break
         self.end_Recording()
 
+    #this function ends the recording successfully
     def end_Recording(self):
         cv2.destroyAllWindows()
         self.out.release()
